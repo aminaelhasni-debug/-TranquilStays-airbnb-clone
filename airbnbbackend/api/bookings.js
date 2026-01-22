e9b2fd0093572e75f6d0639a2c2d3d62accd8590
aminaelhasni-debug <aminaelhasni17@gmail.com>
2026-01-22 09:54:28 +0100

    modif9

--- airbnbbackend/api/bookings.js
+++
@@ -1,8 +0,0 @@
-import { getBookings, createBooking } from "../controllers/bookingsController";
-
-export default async function handler(req, res) {
-  if (req.method === "GET") return getBookings(req, res);
-  if (req.method === "POST") return createBooking(req, res);
-  return res.status(405).json({ message: "Method not allowed" });
-}
-
